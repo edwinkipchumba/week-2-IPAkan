f124088552cef89604b494081fede63d57a04d90
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-07-17 16:35:45 +0300

    variable declaration

---
+++ index.js
@@ -0,0 +1,4 @@
+var CC, YY, MM, d, dayValue;
+var dayNames= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
+var maleNames= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleNames= ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
