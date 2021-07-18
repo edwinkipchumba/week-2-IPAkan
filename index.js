d6f51e1fb4c4854102fb29c7188931135ab9b23b
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-07-18 09:34:47 +0300

    added media query for responiveness

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-var CC, YY, MM, DD; d; dayValue;
+var CC, YY, MM, DD, d, dayValue;
 var dayNames= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday"];
 var maleNames= ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 var femaleNames= ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 function calculateDayValue(){
-   CC = parseInt
-   YY = parseInt
-   MM = parseInt
-   DD = parseInt
+   CC = parseInt(year.subString(0,2))
+   YY = parseInt(year.subString(2,4))
+   MM = parseInt(document.getElementById("month").value);
+   DD = parseInt(document.getElementById("date").value);
    d =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7
 }
 /*---form validation---*/
-function validateInPutForm(){
-    var gender = document.getElementsByName("gender");
-    if(document.inPutForm.year.value==="" || document.inPutForm.year.value.length !=4 || document.inPutForm.year.value>2500 || document.inPutForm.year.value<=1800){
+function validate(){
+    var genders = document.getElementsByName("gender");
+    if(document.inPutForm.year.value=="" || document.inPutForm.year.value.length !=4 || document.inPutForm.year.value>2300 || document.inPutForm.year.value<=1800){
         alert("please provide a valid year e.g 2021");
         document.inPutForm.year.focus();
+        return false;
     }
 
 };
-  else if(document.inPutForm.month.value==="" || isNaN(document.inPutForm.month.value) || document.inPutForm.month.value.length !=2 || document.inPutForm.month.value>12 || document.inPutForm.month.value<=0){
+  else if(document.inPutForm.month.value=="" || isNaN(document.inPutForm.month.value) || 
+  document.inPutForm.month.value.length !=2 || document.inPutForm.month.value>12 || document.inPutForm.month.value<=0){
      alert("please provide a valid month from 1 to 12");
      document.inPutForm.month.focus();
+     return false;
  }
- else if(document.inPutForm.day.value==="" || isNaN(document.inPutForm.day.value) || document.inPutForm.day.value.length !=2 || document.inPutForm.day.value>31 || document.inPutForm.day.value<=0){
+ else if(document.inPutForm.day.value=="" || isNaN(document.inPutForm.day.value) || 
+ document.inPutForm.day.value.length !=2 || document.inPutForm.day.value>31 || document.inPutForm.day.value<=0){
      alert("please provide a valid days from 1 to 31");
      document.inPutForm.day.focus();
+     return false;
  };
  else if(gender[0].checked===false && gender[1].checked===false){
      alert("you must select either male or female gender");
@@ -70,7 +75,7 @@ switch(gender().getGender()){
      else if(dayValue===6){
          alert("you were born on" + dayNames[5] + " and your akan name is " + maleNames[5] + "!");
      }
-     else if(dayValue===7){
+     else if(dayValue===-0){
          alert("you were born on" + dayNames[6] + " and your akan name is " + maleNames[6] + "!");
      }
      break;
@@ -93,7 +98,7 @@ switch(gender().getGender()){
         else if(dayValue===6){
          alert("you were born on" + dayNames[5] + " and your akan name is " + femaleNames[5] + "!");
         }
-        else if(dayValue===7){
+        else if(dayValue===-0){
         alert("you were born on" + dayNames[6] + " and your akan name is" + femaleNames[6] + "!");
         }
         break
